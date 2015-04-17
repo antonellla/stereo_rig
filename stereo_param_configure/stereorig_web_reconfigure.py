fca07e6be2cf5d3ac31e343c471de297e5c1c691
Yifei Zhang <njzhangyifei@gmail.com>
2015-04-17 16:41:42 -0700

    added a new stereorig web reconfigure python script for testing

---
+++ stereo_param_configure/stereorig_web_reconfigure.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id()+ "I got %d", data.data);
+
+def listener():
+    rospy.init_node('stereorig_web_reconfigure', anonymous=True)
+    rospy.loginfo('Stereo Rig Web Reconfigure up and running')
+
+    rospy.Subscriber('exposure',Float64, queue_size=10)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
