3fd4bb0083370292180d78dda8dadb289a327b25
Yifei Zhang <njzhangyifei@gmail.com>
2015-04-18 01:12:00 -0700

    Created a package for stereorig_web_reconfigure

--- src/stereo_param_configure/scripts/stereorig_web_reconfigure.py
+++ src/stereo_param_configure/scripts/stereorig_web_reconfigure.py
@@ -2,13 +2,13 @@
 import rospy
 from std_msgs.msg import Float64
 
-def callback(data)
+def callback(data):
     rospy.loginfo(rospy.get_caller_id()+ "I got %d", data.data);
 
 def listener():
     rospy.init_node('stereorig_web_reconfigure', anonymous=True)
     rospy.loginfo('Stereo Rig Web Reconfigure up and running')
-    rospy.Subscriber('exposure',Float64, queue_size=10)
+    rospy.Subscriber('/exposure',Float64, queue_size=10)
     rospy.spin()
 
 if __name__ == '__main__':
